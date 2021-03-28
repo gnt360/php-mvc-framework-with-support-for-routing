1ebce2061e9b58c931bb336967ae85f82b2ac05b
ITguy <paullordz@gmail.com>
2021-03-28 08:46:46 +0100

    views rendering added

--- public/index.php
+++ public/index.php
@@ -4,10 +4,10 @@ use app\core\Application;
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-$app = new Application ();
+$app = new Application (dirname(__DIR__));
 
-$app->router->get('/', function(){
-    return 'Hello World';
-});
+$app->router->get('/', 'home');
+
+$app->router->get('/login', 'login');
 
 $app->run();
